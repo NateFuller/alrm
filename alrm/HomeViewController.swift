0a6dd0cd936c6f700e6c41330a21f54996bc1b84
NateFuller <fatenuller@gmail.com>
2015-12-20 21:42:56 -0500

    Initial commit

---
+++ alrm/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alrm
+//
+//  Created by Nathan Fuller on 12/20/15.
+//  Copyright © 2015 Nathan Fuller. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
